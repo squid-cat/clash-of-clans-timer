5f03f6471a4a0cbaddd96ec8b6cfaa701b19c107
squid-cat <kaikai120626@gmail.com>
2023-08-13 10:32:58 +0900

    add: 日付表記を追加

--- components/Clock.tsx
+++ components/Clock.tsx
@@ -8,11 +8,13 @@ export default function Clock(props: Props) {
 
   const DisplayTime = styled.h2``;
 
+  const strYear = props.now.getFullYear().toString()
+  const strMonth = (props.now.getMonth() + 1).toString().padStart(2, "0")
+  const strDay = props.now.getDate().toString().padStart(2, "0")
+
   const strHours = props.now.getHours().toString().padStart(2, "0")
   const strMinutes = props.now.getMinutes().toString().padStart(2, "0")
   const strSeconds = props.now.getSeconds().toString().padStart(2, "0")
 
-  return (<>
-    <DisplayTime>{strHours}:{strMinutes}:{strSeconds}</DisplayTime>
-  </>)
+  return <DisplayTime>{strYear}/{strMonth}/{strDay} {strHours}:{strMinutes}:{strSeconds}</DisplayTime>
 }
