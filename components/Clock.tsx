d7c10c6a3ccec58eb585db3800ee2771724d8111
squid-cat <kaikai120626@gmail.com>
2023-08-13 12:46:55 +0900

    chore: linter

--- components/Clock.tsx
+++ components/Clock.tsx
@@ -1,22 +1,25 @@
-import { styled } from 'styled-components'
+import { styled } from "styled-components";
 
 type Props = {
-  now: Date
-}
+  now: Date;
+};
+
+const DisplayTime = styled.h2`
+  font-weight: normal;
+`;
 
 export default function Clock(props: Props) {
+  const strYear = props.now.getFullYear().toString();
+  const strMonth = (props.now.getMonth() + 1).toString().padStart(2, "0");
+  const strDay = props.now.getDate().toString().padStart(2, "0");
 
-  const DisplayTime = styled.h2`
-    font-weight: normal;
-  `;
+  const strHours = props.now.getHours().toString().padStart(2, "0");
+  const strMinutes = props.now.getMinutes().toString().padStart(2, "0");
+  const strSeconds = props.now.getSeconds().toString().padStart(2, "0");
 
-  const strYear = props.now.getFullYear().toString()
-  const strMonth = (props.now.getMonth() + 1).toString().padStart(2, "0")
-  const strDay = props.now.getDate().toString().padStart(2, "0")
-
-  const strHours = props.now.getHours().toString().padStart(2, "0")
-  const strMinutes = props.now.getMinutes().toString().padStart(2, "0")
-  const strSeconds = props.now.getSeconds().toString().padStart(2, "0")
-
-  return <DisplayTime>{strYear}/{strMonth}/{strDay} {strHours}:{strMinutes}:{strSeconds}</DisplayTime>
+  return (
+    <DisplayTime>
+      {strYear}/{strMonth}/{strDay} {strHours}:{strMinutes}:{strSeconds}
+    </DisplayTime>
+  );
 }
