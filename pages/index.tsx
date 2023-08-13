b296b58472be33a17bec0dc62b36b45b5246136e
squid-cat <kaikai120626@gmail.com>
2023-08-13 12:46:44 +0900

    feat: タイマーの作成

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,33 +1,47 @@
-import Clock from '@/Clock';
-import Timer from '@/Timer'
-import { useEffect, useState } from 'react';
-import { styled } from 'styled-components'
+import Clock from "@/Clock";
+import InputTimer from "@/InputTimer";
+import { useEffect, useState } from "react";
+import { styled } from "styled-components";
+
+const Wrapper = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 40px;
+`;
+
+const SectionTitle = styled.h2`
+  margin: 0;
+`;
 
 export default function Home() {
-
-  const Wrapper = styled.section`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    margin-top: 40px;
-  `;
-
-  const Title = styled.h2`
-    margin: 0;
-  `;
-
   const [date, setDate] = useState(new Date());
 
   useEffect(() => {
-    setInterval(() => {
+    const interval = setInterval(() => {
       setDate(new Date());
     }, 1000);
+
+    return () => clearInterval(interval);
   }, []);
 
   return (
-    <Wrapper>
-      <Title>現在時刻</Title>
-      <Clock now={date} />
-    </Wrapper>
-  )
+    <>
+      <Wrapper>
+        <SectionTitle>現在時刻</SectionTitle>
+        <Clock now={date} />
+
+        <SectionTitle>大工タイマー</SectionTitle>
+        <InputTimer id={1} />
+        <InputTimer id={2} />
+        <InputTimer id={3} />
+        <InputTimer id={4} />
+        <InputTimer id={5} />
+        <InputTimer id={6} />
+
+        <SectionTitle>ラボ</SectionTitle>
+        <InputTimer id={7} />
+      </Wrapper>
+    </>
+  );
 }
